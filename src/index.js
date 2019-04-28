73d2ba9a14211934563723b37cc4e643bca3d458
azamkhanuk <azamkhandev@gmail.com>
2019-04-28 15:43:11 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+
+const toyCollection = document.querySelector('#toy-collection')
+const formEL = document.querySelector('#add-toy-form')
+
 let addToy = false
 
-// YOUR CODE HERE
+// Add Toy Button 
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +19,84 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+// Display all toys
+const displayToy = toy => {
 
-// OR HERE!
+  let div = document.createElement('div') 
+      div.className = "card"
+      div.innerHTML = 
+  `
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p class='likes>${toy.likes} Likes </p>
+        <button class="like-btn">Like <3</button>
+  `
+  toyCollection.append(div)
+  //like button 
+  const likeBtn = div.querySelector('.like-btn')
+  likeBtn.addEventListener('click', () => {
+    toy.likes++
+    likeBtn.previousElementSibling.innerHTML = `${toy.likes} Likes`
+  })
+}
+
+// Create a toy in the server
+
+formEL.addEventListener('submit', event => {
+    event.preventDefault()
+    const toy = {
+      name: formEL.name.value,
+      image: formEL.image.value,
+      likes : 0
+    }
+    renderToy(toy)
+    formEL.reset()
+})
+
+createBook(book)
+.then(addBook)
+
+
+const createToy = toy => 
+  fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(toy)
+  }).then(resp => resp.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Grabbing and displaying data from server
+
+const getToys = () =>
+	fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+
+const displayToys = toys => {
+	toys.forEach(displayToy)
+}
+
+getToys()
+  .then(displayToys)
+
+
+  // const likeBtn = div.querySelector('.like-btn')
+  // likeBtn.addEventListener('click', () => {
+  //   toy.likes++
+  //   likeBtn.previousElementSibling.innerHTML = `${toy.likes} Likes`
+  // })
